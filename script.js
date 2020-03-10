3b1f7c8ec6ffaff623b3ffb552e121a76c4d5006
154852 <louisemileploix@icloud.com>
2020-03-10 17:12:56 +0000

    Added transitions to elements

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let fuse, equations;
 const vue = new Vue({
-    "el": "#app",
+    el: "#app",
     data: {
         equations: [],
         input: "",
@@ -8,35 +8,43 @@ const vue = new Vue({
     },
     methods: {
         search: function() {
+            let search = fuse.search(vue.input);;
+            let toRemove = vue.equations.filter((x) => search.find((y) => y.id == x.id) == null);
+            let toAdd = search.filter((x) => vue.equations.find((y) => y.id == x.id) == null);
+            toAdd.forEach((x) => vue.equations.splice(0, 0, x));
+            toRemove.forEach((eqn) => {
+                vue.equations.splice(vue.equations.findIndex((x) => x.id == eqn.id), 1);
+            });
             vue.focused = null;
-            vue.equations = fuse.search(vue.input);
         },
         focus: function(eqn) {
             vue.focused = eqn;
-            Vue.nextTick().then(function () {
+            Vue.nextTick().then(function() {
                 renderMathInElement(document.body);
             });
         }
     }
 });
 
-axios.get("/equations.json").then((response) => {
+axios.get("./equations.json").then((response) => {
     equations = response.data.equations;
-    equations.forEach((equation) => {
+    equations.forEach((equation, id) => {
         equation.latex = katex.renderToString(equation.latex, {
             output: "html",
             displayMode: true,
             throwOnError: false
         });
+        equation.id = id;
     });
 
     fuse = new Fuse(equations, {
         keys: [{
             name: "name",
-            weight: 0.7
+            weight: 0.1
         }, {
             name: "tags",
-            weight: 0.3
-        }]
+            weight: 0.9
+        }],
+        threshold: 0.1
     });
 });
